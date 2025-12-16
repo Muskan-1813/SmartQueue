1a624416ece1064b321fdc889376d387870cdf9a
Rishi <rishikm215@gmail.com>
2025-12-16 19:41:37 +0530

    blocked cors and Switched to routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
-import authRoutes from './routes/auth.js'
+import queueRoutes from './routes/queue.js'
+import mongoose from "mongoose";
 const app = express();
 const port = 8080;
 
@@ -10,12 +11,16 @@ app.use(express.json())
 app.listen(port, () => {
   console.log("listening on port" + `http://localhost:${port}`);
 });
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
-  allowedHeaders: ["Content-Type", "Authorization"]
-}))
+
+mongoose.connect("mongodb://127.0.0.1:27017/SmartQueue")
+.then(()=>console.log("connected with database"))
+.catch((err)=>console.log(err));
+// app.use(cors({
+//   origin:"http://localhost:5173",
+//   credentials:true,
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }))
 app.get("/api", (req, res) => {
   res.status(201).json("landing page");
 });
-app.use("/api",authRoutes);
+app.use("/api",queueRoutes);
